602aa9eb1d53257ca1eeb8474313a673070fd957
Dev Patel <devarshi.ap@gmail.com>
2022-09-26 21:50:10 -0400

    remove: sql for now

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,18 +20,18 @@ ChartJS.register(
 );
 
 export const data = {
-    labels: ['React', 'Python', 'Java', 'Node.js (+Express)', 'SQL', 'Next.js'],
+    labels: ['React', 'Python', 'Java', 'TypeScript', 'Next.js', 'SQL', 'Node.js (+Express)'],
     datasets: [
       {
         label: 'Lines of Code [scale= 1:1000]',
-        data: [7, 8, 8, 6, 1, 3],
+        data: [7, 8, 8, 3, 3, 1, 6],
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1,
       },
       {
         label: 'Proficiency [scale= 1:1 out of /10]',
-        data: [8, 8, 8, 6, 4, 6],
+        data: [8, 8, 8, 7, 6, 4, 6],
         backgroundColor: 'rgba(85, 223, 248, 0.2)',
         borderColor: 'rgba(85, 223, 248, 1)',
         borderWidth: 1,
@@ -76,7 +76,34 @@ export default function AboutPage() {
                             I'm always learning new skills and expanding my knowledge by taking up projects that involve <span className="text-orange dark:text-twitter">bleeding-edge technology</span>.
                             Actively in pursuit of a 2023 internship (pretty pls :)).
                         </p>
+                    </div>
+                    
+                    <h1 className="text-4xl mobile:text-2xl mt-10 my-2 font-SerifPro text-orange dark:text-twitter underline underline-offset-6">Skills</h1>
+                    
+                    <h3 className="text-xl mobile:text-lg my-1 font-SansPro text-gray dark:text-d_coolwhite">$ Languages & Frameworks</h3>
+                    <div className="grid grid-cols-4 gap-4 [&>*]:animate-bounce-slow text-center text-gray dark:text-d_coolwhite border-2 border-[#60a5fa] pt-1">
+                        {languages}
+                    </div>
 
+                    <h3 className="text-xl mobile:text-lg my-1 font-SansPro text-gray dark:text-d_coolwhite">$ Databases & Stores</h3>
+                    <div className="grid grid-cols-3 gap-4 [&>*]:animate-bounce-slow text-center text-gray dark:text-d_coolwhite border-2 border-[#8b22cc] pt-1">
+                        {databases}
+                    </div>
+
+                    <h3 className="text-xl mobile:text-lg my-1 font-SansPro text-gray dark:text-d_coolwhite">$ Tools</h3>
+                    <div className="grid grid-cols-2 gap-4 [&>*]:animate-bounce-slow text-center text-gray dark:text-d_coolwhite border-2 border-[#e883d8] pt-1">
+                        {tools}
+                    </div>
+
+                    <h3 className="text-xl mobile:text-lg my-1 font-SansPro text-gray dark:text-d_coolwhite">$ Other</h3>
+                    <div className="grid grid-cols-2 gap-4 [&>*]:animate-bounce-slow text-center text-gray dark:text-d_coolwhite border-2 border-[#f43f5e] pt-1">
+                        {otherStuff}
+                    </div>
+
+                    <Radar data={data} className="border-2 p-2 my-5 bg-white rounded-md" />
+
+                    <h1 className="text-4xl mobile:text-2xl mt-10 my-2 font-SerifPro text-orange dark:text-twitter underline underline-offset-6">Interests & Hobbies</h1>
+                    <div className='text-dark_gray dark:text-c_taupe text-md font-SansPro space-y-6'>
                         <p>
                             It's important to discover things you enjoy outside of school and work; things that make you human!
                             Some activities I enjoy the heck out of:
@@ -104,30 +131,6 @@ export default function AboutPage() {
                             </ul>
                         </ul>
                     </div>
-                    
-                    <h1 className="text-4xl mobile:text-2xl mt-10 my-2 font-SerifPro text-orange dark:text-twitter underline underline-offset-6">Skills</h1>
-                    
-                    <h3 className="text-xl mobile:text-lg my-1 font-SansPro text-gray dark:text-d_coolwhite">$ Languages & Frameworks</h3>
-                    <div className="grid grid-cols-4 gap-4 [&>*]:animate-bounce-slow text-center text-gray dark:text-d_coolwhite border-2 border-[#60a5fa] pt-1">
-                        {languages}
-                    </div>
-
-                    <h3 className="text-xl mobile:text-lg my-1 font-SansPro text-gray dark:text-d_coolwhite">$ Databases & Stores</h3>
-                    <div className="grid grid-cols-3 gap-4 [&>*]:animate-bounce-slow text-center text-gray dark:text-d_coolwhite border-2 border-[#8b22cc] pt-1">
-                        {databases}
-                    </div>
-
-                    <h3 className="text-xl mobile:text-lg my-1 font-SansPro text-gray dark:text-d_coolwhite">$ Tools</h3>
-                    <div className="grid grid-cols-2 gap-4 [&>*]:animate-bounce-slow text-center text-gray dark:text-d_coolwhite border-2 border-[#e883d8] pt-1">
-                        {tools}
-                    </div>
-
-                    <h3 className="text-xl mobile:text-lg my-1 font-SansPro text-gray dark:text-d_coolwhite">$ Other</h3>
-                    <div className="grid grid-cols-2 gap-4 [&>*]:animate-bounce-slow text-center text-gray dark:text-d_coolwhite border-2 border-[#f43f5e] pt-1">
-                        {otherStuff}
-                    </div>
-
-                    <Radar data={data} className="border-2 p-2 my-5 bg-white rounded-md" />
                 </div>
             </div>
         </Layout>
