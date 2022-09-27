f0da2c75b0ffb886444e50e015034b7d7f6ce0ba
Dev Patel <devarshi.ap@gmail.com>
2022-09-26 21:50:36 -0400

    rename file

---
+++ utils/utils-data.ts
@@ -0,0 +1,65 @@
+export const projects = [
+    {
+        name:'Skool',
+        description:'Type-safe REST-API to perform CRUD ops on a school-db model.',
+        tools:'[TypeScript, NextJs, Tailwind, Prisma, Postgres]',
+        link:'https://github.com/devarshi-ap/skool',
+        demo:false
+    },
+    {
+        name:'Monte-Carlo Poker',
+        description:'Monte Carlo Simulator for Texas-Holdem used to calculate Poker hand probabilities.',
+        tools:'[Python, SQLite3, Seaborn, Unittest]',
+        link:'https://github.com/devarshi-ap/MonteCarloPoker',
+        demo:false
+    },
+    {
+        name:'Psyyy',
+        description:'Personal budgeting tool that helps keep track of all your expenses.',
+        tools:'[React, Redux-TK, Tailwind, Vite, Cypress (E2E)]',
+        link:"https://psyyy.vercel.app",
+        demo:true
+    },
+    {
+        name:'Wiki-Wow',
+        description:'Enhanced "On This Day" web-app using YouTube & Wikipedia APIs.',
+        tools:'[Next, Tailwind, Axios, Cypress (E2E)]',
+        link:"https://wikiwow.vercel.app",
+        demo:true
+    },
+    // {
+    //     name:'Adoc-Md',
+    //     description:'Markup text editor which supports preview and unidirectional conversion from markdown and asciidoc to HTML.',
+    //     tools:'[JavaScript, Java]',
+    //     link:"https://adoc-md.vercel.app",
+    //     demo:true
+    // },
+    {
+        name:'DSA-Snippets',
+        description:'VS Code extension for JavaScript DS&A code snippets.',
+        tools:'[JavaScript]',
+        link:"https://marketplace.visualstudio.com/items?itemName=DevarshiPatel03.dsasnippets",
+        demo:true
+    },
+    {
+        name:'Weather App',
+        description:'Basic web-app that displays useful meteorological data for a given city using the Openweathermap API.',
+        tools:'[JavaScript, HTML, CSS]',
+        link:"https://weathrweb.netlify.app",
+        demo:true
+    },
+    {
+        name:'Competitive Programming',
+        description:'A competitive programming solutions library for questions from DMOJ.',
+        tools:'[Java, Python]',
+        link:"https://github.com/devarshi-ap/Competitive-Programming",
+        demo:false
+    },
+    {
+        name:'Folder Organizer Script',
+        description:"Python script that organizes a dir by iteratively creating and populating subdir's based on file ext's.",
+        tools:'[Python]',
+        link:"https://github.com/devarshi-ap/Python-Projects/blob/main/Folder%20Organizer/folder_organizer.py",
+        demo:false
+    }
+]
